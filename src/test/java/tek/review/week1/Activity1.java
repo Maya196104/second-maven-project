968b50512936b5ef727e1a535b3f4ed0b5cd55f8
maya <maya.alieva.git@tekschool.us>
2024-07-27 00:05:40 -0500

    Create account

---
+++ src/test/java/tek/review/week1/Activity1.java
@@ -0,0 +1,81 @@
+package tek.review.week1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity1 {
+
+
+    /*
+    In Retail app, click on sign in then click on Create new Account
+    then fill up the form and sign up. Expectation is to Create new Account.
+    once account created make sure profile picture is displayed. (isDisplayed)
+    And print result of isDisplayed method.
+    Push to your repository
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+        driver.get("https://retail.tekschool-students.com/");
+
+        //Click on SignIn then click
+        By signInLocator = By.id("signinLink");
+        WebElement signInElement = driver.findElement(signInLocator);
+        signInElement.click();
+
+           //Create new Account
+       By newAccountLocator = By.id("newAccountBtn");
+       WebElement newAccountElement = driver.findElement(newAccountLocator);
+       newAccountElement.click();
+
+        //fill up the form
+        By nameLocator = By.id("nameInput");
+        WebElement nameElement = driver.findElement(nameLocator);
+        nameElement.sendKeys("Dalerjon");
+
+        //Random
+        String emailPrefix = "Dalerjon_Spartan";
+        int number = (int) (Math.random() * 100);
+        String randomEmail = emailPrefix + number + "@gmail.com";
+
+           //fill up the form
+        By emailLocator = By.id("emailInput");
+        WebElement emailElement =  driver.findElement(emailLocator);
+        emailElement.sendKeys(randomEmail);
+
+          //fill up the form
+        By passwordLocator = By.id("passwordInput");
+        WebElement passwordElement = driver.findElement(passwordLocator);
+        passwordElement.sendKeys("Alijon03@");
+
+        //fill up the form
+        By confirmPassword = By.id("confirmPasswordInput");
+        WebElement confirmPasswordElement =  driver.findElement(confirmPassword);
+        confirmPasswordElement.sendKeys("Alijon03@");
+
+        By signupLocator = By.id("signupBtn");
+        WebElement signupElement =  driver.findElement(signupLocator);
+        signupElement.click();
+
+        // profile picture is displayed
+        Thread.sleep(3000);
+        By profileLocator = By.id("profileImage");
+        WebElement profileElement = driver.findElement(profileLocator);
+        boolean isProfileDisplayed = profileElement.isDisplayed();
+         if (isProfileDisplayed){
+             System.out.println("Test Passed");
+         } else {
+             System.out.println("Test Failed");
+         }
+
+       driver.quit();
+
+    }
+
+}
---
+++ src/test/java/tek/review/week1/RandomNumber.java
@@ -0,0 +1,19 @@
+package tek.review.week1;
+
+public class RandomNumber {
+
+    public static void main(String[] args) {
+
+    //0 - 1
+
+       // double random = Math.random();
+       // int number = (int) (random *1000);
+
+        //System.out.println(number);
+
+        int number = (int) (Math.random() *1000);
+
+        System.out.println(number);
+
+    }
+}
